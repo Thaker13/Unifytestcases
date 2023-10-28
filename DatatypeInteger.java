768b95162173a9a09a2e014ad54cdc8f37505c7b
Neha Thaker <thakersmail@yahoo.co.uk>
2023-10-28 12:24:06 +0100

    this is my first trial file

---
+++ DatatypeInteger.java
@@ -0,0 +1,17 @@
+package datatypeinteger;
+
+public class DatatypeInteger {
+
+    public static void main (String[]args) {
+
+        int A = 10;
+        int B = 5;
+
+        System.out.println ( A + B );
+        System.out.println ( A - B );
+        System.out.println ( A * B );
+        System.out.println ( A / B );
+        System.out.println ( A ++ );
+        System.out.println ( B -- );
+    }
+}
